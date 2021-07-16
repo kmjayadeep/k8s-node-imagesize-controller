11cd5ece883538ea723231a85e1a30fd1782a27e
Jayadeep KM <kmjayadeep@gmail.com>
2021-07-16 09:42:50 +0530

    complete watcher

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/kmjayadeep/simplecontroller
 go 1.16
 
 require (
+	github.com/dustin/go-humanize v1.0.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.21.2
+	k8s.io/apimachinery v0.21.2
 	k8s.io/client-go v0.21.2
 )
--- main.go
+++ main.go
@@ -1,60 +1,106 @@
 package main
 
 import (
-  "os"
+	"context"
+	"fmt"
+	"os"
 
-  "github.com/urfave/cli/v2"
-  "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/tools/clientcmd"
-  "k8s.io/client-go/rest"
-  log "github.com/sirupsen/logrus"
+	"github.com/dustin/go-humanize"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+	coreV1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+var imageSizes map[string]uint64
+
 func main() {
-  app := &cli.App{
-    Name: "Controller",
-    Usage: "A simple kubernetes controller",
-    Flags: []cli.Flag{
-      &cli.StringFlag{
-        Name: "config",
-        Usage: "kubeconfig path for usage outside k8s cluster",
-      },
-    },
-  }
+	app := &cli.App{
+		Name:  "Controller",
+		Usage: "A simple kubernetes controller",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "config",
+				Usage: "kubeconfig path for usage outside k8s cluster",
+			},
+		},
+	}
 
-  app.Action = func(c *cli.Context) error {
-    config := c.String("config")
-    log.WithField("config", config).Info("starting controller")
+	app.Action = func(c *cli.Context) error {
+		config := c.String("config")
+		log.WithField("config", config).Info("starting controller")
 
-    _, err := getClient(config)
+		client, err := getClient(config)
 
-    if err != nil {
-      return err
-    }
+		if err != nil {
+			return err
+		}
 
-    return nil
-  }
+		err = watchNodes(client)
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+		return err
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func watchNodes(clientset *kubernetes.Clientset) error {
+	api := clientset.CoreV1().Nodes()
+	watcher, err := api.Watch(context.Background(), metav1.ListOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	ch := watcher.ResultChan()
+
+	for event := range ch {
+		node, ok := event.Object.(*coreV1.Node)
+		if !ok {
+			watcher.Stop()
+			return fmt.Errorf("couldn't cast to nodelist")
+		}
+
+		checkImageSize(node)
+	}
+
+	return nil
+}
+
+func checkImageSize(node *coreV1.Node) {
+
+	var size uint64 = 0
+
+	for _, img := range node.Status.Images {
+		size = size + uint64(img.SizeBytes)
+	}
+
+	if imageSizes[node.Name] != size {
+		log.Infof("Image size changed for node %s. Old: [%v], New: [%v]", node.Name, humanize.Bytes(imageSizes[node.Name]), humanize.Bytes(size))
+	}
+
 }
 
 func getClient(config string) (*kubernetes.Clientset, error) {
-  var cfg *rest.Config
-  var err error
-  if config == "" {
-    // in cluster config
-    cfg, err = rest.InClusterConfig()
-    if err != nil {
-      return nil, err
-    }
-  }
+	var cfg *rest.Config
+	var err error
+	if config == "" {
+		// in cluster config
+		cfg, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	cfg, err = clientcmd.BuildConfigFromFlags("", config)
 	if err != nil {
-    return nil, err
+		return nil, err
 	}
 
 	return kubernetes.NewForConfig(cfg)
